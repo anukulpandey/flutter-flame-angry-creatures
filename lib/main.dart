af54cbcb8c9b08dca69a1c047bc5804543d829c4
Anukul Pandey <anukul.030601@gmail.com>
2023-04-07 21:43:38 +0530

    feat: player

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,14 @@ void main() {
 class MyGame extends Forge2DGame{
 
 }
+
+
+class Player extends BodyComponent{
+  @override
+  Body createBody() {
+    Shape shape = CircleShape()..radius = 3;
+    BodyDef bodyDef = BodyDef(position: Vector2(10,5),type: BodyType.dynamic); //this can interact with others
+    FixtureDef fixtureDef = FixtureDef(shape,friction: 0.4,density: 1); // determines physical properties
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
+  }
+}
